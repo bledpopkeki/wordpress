fdfb200c65f4ec49aa1a6aa6cd5cbba736304e9d
bledpopkeki <bledh4352@gmail.com>
2025-06-10 07:03:08 -0700

    bhjjgh

--- functions.php
+++ functions.php
@@ -6,7 +6,28 @@ function load_scripts(){
         array(),
         filemtime(get_template_directory(). '/style.css'),'all'
         );
-    
+    wp_enqueue_script('dropdown',get_template_directory().'/js/dropdown.js',array(),'1.0',false);
 }
 add_action('wp_enqueue_scripts','load_scripts');
+function config(){
+    register_nav_menus(
+    array(
+        'wp_devs_main_menu'=>'Main Menu',
+        'wp_devs_footer_menu' => 'Footer Menu'
+    )
+    );
+    $args=array(
+        'height'=>225,
+        'width'=>1920
+    );
+    add_theme_support('custom-header',$args);
+    add_theme_support('post_thumbnails');
+    add_theme_support('custom-logo',array(
+        'width'=>200,
+        'height'=>110,
+        'flex-height'=>true,
+        'flex-width'=>true
+    ));
+}
+add_action('after_setup_theme','config',0);
 ?>
